bc01c62d1fc419bf896d2702f63e1418653b67a0
Vyshnaviprasee <vyshnaviprasee@gmail.com>
2024-05-17 15:19:07 +0530

    updated

--- myecommerce/orders/admin.py
+++ myecommerce/orders/admin.py
@@ -3,7 +3,16 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order_status', 'status', 'owner')
+    list_filter = (
+        'owner', 
+        'status',
+        'order_status',
+        )
+    search_fields = ('id', 'owner', 'status', 'order_status')
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderedItem)
 
 
